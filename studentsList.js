b3f1c428998031177ff57febc65e97e3ee2c6e95
philboud <philippe.boudet42@gmail.com>
2017-03-06 12:29:47 +0100

    modification json

--- studentsList.js
+++ studentsList.js
@@ -1,13 +1,30 @@
 // Write yout Javascript code in this files
 var request = new XMLHttpRequest();
+var table =document.getElementById('students');
+
 request.open('GET', 'students.json', true);
+
 request.onreadystatechange = function() {
     if (request.readyState == 4) {
         if (request.status == 200) {
             var students = JSON.parse(request.responseText);
-            console.log(students);
+
+            students.forEach(function (student) {
+
+              let tr = document.createElement('tr');
+
+              tr.innerHTML = `<td>${student.firstname}</td>
+                              <td>${student.firstname}</td>
+                              <td>${student["postal-code"]}</td>`;
+
+            table.appendChild(tr);
+
+          });
+
         } else {
+
             dump("Erreur pendant le chargement de la page.\n");
+
         }
     }
 };
